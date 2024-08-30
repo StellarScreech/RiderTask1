b01bc6fbbf8620eb0f3d591a4e7e9eda72cba919
lazarev_a <lazarev_a@auca.kg>
2024-08-30 18:18:14 +0600

    Cars removed.

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,14 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-public class Contact
+﻿public class Contact
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string LastName { get; set; }
-    public string MiddleName { get; set; }
-    public string PhoneNumber { get; set; }
-    
-    public string CarNumber { get; set; }
-    public string CarModel { get; set; }
+    public string? MiddleName { get; set; }
+    public string? PhoneNumber { get; set; }
 }
